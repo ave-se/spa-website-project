c05857a85c4f753f95fe8037630c9b5bfc565fa4
agnes-cybil atsuah <agnescybil.atsuah@hyperisland.se>
2024-06-09 20:20:28 +0200

    Included the task prioritization feature

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,9 +3,11 @@ import React from 'react';
 import TaskItem from './TaskItem';
 
 function TaskList({ tasks, onDelete, onToggle }) {
+  const sortedTasks = [...tasks].sort((a, b) => a.priority - b.priority);
+
   return (
     <>
-      {tasks.map((task) => (
+      {sortedTasks.map((task) => (
         <TaskItem key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
       ))}
     </>
