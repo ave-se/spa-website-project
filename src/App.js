83b6216034df163e317cff2226446fcd6c448197
agnes-cybil atsuah <agnescybil.atsuah@hyperisland.se>
2024-06-09 19:20:39 +0200

    Basic code setup for header components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-
+import TaskList from './components/TaskList';
+import AddTaskForm from './components/AddTaskForm';
 
 function App() {
+  const [tasks, setTasks] = React.useState([
+    {
+      id: 1,
+      text: 'Doctor appointment',
+      day: 'Feb 5th at 2:30pm',
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 1:30pm',
+    },
+  ]);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <Router>
-    <div className="App">
-      <Header />
+      <div className="App">
+        <Header />
         <Routes>
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/" component={Home} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
         </Routes>
-    </div>
+        <AddTaskForm onAdd={addTask} />
+        {tasks.length > 0 ? (
+          <TaskList tasks={tasks} onDelete={deleteTask} />
+        ) : (
+          'No Tasks To Show'
+        )}
+      </div>
     </Router>
   );
 }
